df0b4f45c274baff815f5d2839120f23c7a29cf7
Valentin Querlier <valentinquerlier@gmail.com>
2015-12-18 23:20:04 +0100

    Create exist.php

---
+++ exist.php
@@ -0,0 +1,18 @@
+<?php
+function Exist($url) {
+if (preg_match('#<title>(.+)<\/title>#',file_get_contents($url),$title)){
+	if($title[1] == "Twitter / Account Suspended" OR $title[1] == "Twitter / Compte Suspendu"){
+		return 'This account is suspended';
+}
+ elseif (substr($title[1], -9) == "| Twitter"){
+		return 'This account is not suspended';
+}}
+$file = @fopen($url, 'r');  
+if ($file)  
+{
+  return "Nonexistent account";
+}
+}
+
+echo Exist('https://twitter.com/654545kjjkjgjf');
+?>
